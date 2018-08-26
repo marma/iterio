d51e2f53e180de153cee89e20f29dbab9602cabb
Martin Malmsten <martin@martinmalmsten.net>
2018-08-26 22:46:27 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='iterio', version='0.0.1', description='file-like object from iterable', author='Martin Malmsten', author_email='martin@martinmalmsten.net', packages=[ 'iterio' ])
